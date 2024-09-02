4377801c88b912781c6030051b6bd6554b6af576
Tejas-963 <tcodercpp233@gmail.com>
2024-09-02 12:23:02 +0530

    Add files via upload

---
+++ Spotify/script.js
@@ -0,0 +1,46 @@
+document.querySelector(".ham").addEventListener("click", ()=>{
+    document.querySelector(".left").style.left = "0";
+})
+
+document.querySelector(".close").addEventListener("click", ()=>{
+    document.querySelector(".left").style.left = "-150%";
+})
+
+document.querySelector(".sign").addEventListener("click", ()=>{
+    window.location.href = "https://www.spotify.com/in-en/signup?forward_url=https%3A%2F%2Fopen.spotify.com%2Fsection%2F0JQ5DAuChZYPe9iDhh2mJz";
+})
+
+document.querySelector(".login").addEventListener("click", ()=>{
+    window.location.href = "https://accounts.spotify.com/en-GB/login?continue=https%3A%2F%2Fopen.spotify.com%2Fsection%2F0JQ5DAuChZYPe9iDhh2mJz";
+})
+
+document.querySelector(".album1 ").addEventListener("click", ()=>{
+    window.location.href = "https://open.spotify.com/playlist/37i9dQZF1DX2L0iB23Enbq";
+})
+document.querySelector(".album2 ").addEventListener("click", ()=>{
+    window.location.href = "https://open.spotify.com/playlist/37i9dQZF1DX5Ejj0EkURtP";
+})
+document.querySelector(".album3 ").addEventListener("click", ()=>{
+    window.location.href = "https://open.spotify.com/playlist/37i9dQZF1DX0XUsuxWHRQd";
+})
+document.querySelector(".album4 ").addEventListener("click", ()=>{
+    window.location.href = "https://open.spotify.com/playlist/37i9dQZF1DWZeKCadgRdKQ";
+})
+document.querySelector(".album5 ").addEventListener("click", ()=>{
+    window.location.href = "https://open.spotify.com/playlist/37i9dQZF1DWT5lkChsPmpy";
+})
+document.querySelector(".album6 ").addEventListener("click", ()=>{
+    window.location.href = "https://open.spotify.com/playlist/37i9dQZF1DWWY64wDtewQt";
+})
+document.querySelector(".album7 ").addEventListener("click", ()=>{
+    window.location.href = "https://open.spotify.com/playlist/37i9dQZF1DX4sWSpwq3LiO";
+})
+document.querySelector(".album8 ").addEventListener("click", ()=>{
+    window.location.href = "https://open.spotify.com/playlist/37i9dQZF1DX4WYpdgoIcn6";
+})
+
+document.querySelector(".album9").addEventListener("click", ()=>{
+    window.location.href = "https://open.spotify.com/playlist/37i9dQZF1DX4UtSsGT1Sbe";
+})
+
+
